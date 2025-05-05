d4512fa99ca11b3be07d25cb27cd767f9f4229ab
guillaume normand <guillaume.normand@aldermore.co.uk>
2025-05-05 11:16:40 +0200

    working E2E test with mock server and database

--- src/routes/valuation/__tests__/valuation.test.ts
+++ src/routes/valuation/__tests__/valuation.test.ts
@@ -1,7 +1,17 @@
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
 import { fastify } from '~root/test/fastify';
 import { VehicleValuationRequest } from '../types/vehicle-valuation-request';
+import { afterEach, beforeEach } from 'vitest';
 
 describe('ValuationController (e2e)', () => {
+
+  const mock = new AxiosMockAdapter(axios);
+
+  beforeEach( () => {
+    mock.reset();
+  })
+
   describe('PUT /valuations/', () => {
     it('should return 404 if VRM is missing', async () => {
       const requestBody: VehicleValuationRequest = {
@@ -64,6 +74,9 @@ describe('ValuationController (e2e)', () => {
       const requestBody: VehicleValuationRequest = {
         mileage: 10000,
       };
+
+      mock.onGet("https://run.mocky.io/v3/9245229e-5c57-44e1-964b-36c7fb29168b/valuations/ABC123?mileage=10000").reply(200,
+        { valuation : {lowerValue: 1000, upperValue: 2000 }});
 
       const res = await fastify.inject({
         url: '/valuations/ABC123',
